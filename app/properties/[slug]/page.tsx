61ed89da268364ca1f8fc992953d1ff4fa30c837
mdarbaz <zainshaikh2468@gmail.com>
2025-02-03 20:47:10 +0530

    Add property details layout to the property page

--- app/properties/[slug]/page.tsx
+++ app/properties/[slug]/page.tsx
@@ -1,8 +1,35 @@
 import MainLayout from "@/app/component/layout/MainLayout";
+import Container from "@/app/component/ui/Container";
+import { Grid } from "@/app/component/ui/Grid";
+import Stack from "@/app/component/ui/Stack";
+import Text from "@/app/component/ui/Text";
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 export default function page({}: Props) {
-  return <MainLayout></MainLayout>;
+  return (
+    <MainLayout>
+      <Container>
+        <Stack>
+          <Image src="/images/hero.jpg" width={1920} height={1080} alt="" />
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <Stack>
+              <Text weight={"bold"} size={"2xl"}>
+                Property Details
+              </Text>
+              <p>Property Details</p>
+            </Stack>
+            <Stack>
+              <Text weight={"bold"} size={"2xl"}>
+                Property Details
+              </Text>
+              <p className="text-lg">Property Description</p>
+            </Stack>
+          </div>
+        </Stack>
+      </Container>
+    </MainLayout>
+  );
 }
