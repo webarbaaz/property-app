61ef3094363af8a2b61b2346e49bf6c4330f629d
Nehashaikh913 <nehashaikh0500@gmail.com>
2025-02-09 02:11:50 +0530

    env added

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "pjz2qow2", // Set in .env.local
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // Set in .env.local
   dataset: "production",
   apiVersion: "2023-01-01",
   useCdn: true, // Set to false if you need real-time updates
