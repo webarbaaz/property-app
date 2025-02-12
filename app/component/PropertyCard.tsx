67ecac24e016d5e8ccf8c81fb6863fbf8a8cb925
Nehashaikh913 <nehashaikh0500@gmail.com>
2025-02-13 01:12:37 +0530

    card updated

--- app/component/PropertyCard.tsx
+++ app/component/PropertyCard.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import Stack from "./ui/Stack";
 import Image from "next/image";
 import Text from "./ui/Text";
-import { FaBed, FaLocationDot } from "react-icons/fa6";
-import HStack from "./ui/HStack";
-import Button from "./ui/Button";
 import { Property } from "@/types";
 import generateImageUrl from "@/lib/sanity/utils/imageBuilder";
 import { Grid } from "./ui/Grid";
@@ -17,11 +14,13 @@ export default function PropertyCard({ property }: Props) {
   return (
     <Stack
       spacing={"0"}
-      className="bg-white rounded-lg shadow-md capitalize relative my-4 mx-2">
+      className="bg-white rounded-lg shadow-md capitalize relative my-4 mx-2"
+    >
       <Text
         className="uppercase absolute left-2 top-2 bg-gray-700 px-3 py-1 shadow-lg rounded-full"
         color="white"
-        size={"sm"}>
+        size={"sm"}
+      >
         {property?.propertyStatus}
       </Text>
       <Image
@@ -33,30 +32,32 @@ export default function PropertyCard({ property }: Props) {
       />
       <Stack
         spacing={"1"}
-        className="p-3 rounded-lg absolute left-2 right-2 bg-white bottom-2">
+        className="p-3 rounded-lg absolute left-2 right-2 bg-white bottom-2"
+      >
         <Text weight={"semibold"} className="text-lg">
           {property?.name}
         </Text>
         <Grid cols={2} gap={"md"}>
           <Stack spacing={"1"} className="border-r-2">
             <Text className="text-gray-500">Carpet Area</Text>
-            <Text className="font-semibold">649-1200 sqft</Text>
+            <Text className="font-semibold">{property.carpetArea}</Text>
           </Stack>
           <Stack spacing={"1"}>
             <Text className="text-gray-500">Configuration</Text>
-            <Text className="font-semibold">1 bHK, 2 BHK, 3 BHK</Text>
+            <Text className="font-semibold">{property.size.join(", ")}</Text>
           </Stack>
           <Stack spacing={"1"} className="border-r-2">
             <Text className="text-gray-500">Built up Area</Text>
-            <Text className="font-semibold">On Request</Text>
+            <Text className="font-semibold">{property.buildUpArea}</Text>
           </Stack>
           <Stack spacing={"1"}>
             <Text className="text-gray-500">Possession Date</Text>
-            <Text className="font-semibold">June 2027 </Text>
+            <Text className="font-semibold">{property.possessionDate}</Text>
           </Stack>
         </Grid>
         <Text weight={"semibold"} className="text-lg">
-          INR 2.74 cr <span className="text-sm text-gray-400">Onwards</span>
+          INR {property.price}{" "}
+          <span className="text-sm text-gray-400">Onwards</span>
         </Text>
         {/* <Text size={"sm"} color="secondary">
           {property?.size}
